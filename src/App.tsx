2f0a187f93772165cca12030acb4b08ab8afeac4
Gabriel Gutierez <gabriel@gabriel-inspiron-14-3467>
2022-02-13 22:14:39 -0300

    feat: graphic added with no style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "./components/forms";
 import ResponseData from "./components/responseData";
+import Graph from "./components/resultGraph";
 import "./styles/global.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Form />
       <ResponseData />
+
     </>
   );
 }
--- src/components/responseData/index.tsx
+++ src/components/responseData/index.tsx
@@ -1,21 +1,31 @@
 import { UseSimulationContext } from "../../providers/simulationProvider";
 import Card from "../card/index";
+import Graph from "../resultGraph";
 import "./style.css";
 const ResponseData = () => {
   const { simulateData } = UseSimulationContext();
   return (
     <div id="response-data">
       {simulateData ? (
-        simulateData.map((item: any) => {
+        simulateData.map((item: any, index: number) => {
           return (
-            <>
+            <div id="result-box" key={index}>
               <div id="title">
                 <h2>Resultado da simulação</h2>
               </div>
               <div id="cards-box">
-                <Card title="Valor Final Bruto" value={item.valorFinalBruto} />
-                <Card title="Alíquota do IR" value={item.aliquotaIR} />
-                <Card title="Valor Pago em IR" value={item.valorPagoIR} />
+                <Card
+                  title="Valor Final Bruto"
+                  value={item.valorFinalBruto}
+                />
+                <Card
+                  title="Alíquota do IR"
+                  value={item.aliquotaIR}
+                />
+                <Card
+                  title="Valor Pago em IR"
+                  value={item.valorPagoIR}
+                />
                 <Card
                   title="Valor Final Líquido"
                   value={item.valorFinalLiquido}
@@ -31,7 +41,9 @@ const ResponseData = () => {
                   value={item.ganhoLiquido}
                 />
               </div>
-            </>
+              <Graph/>
+
+            </div>
           );
         })
       ) : (
